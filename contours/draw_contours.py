eff1818bf14c8d7cee2ad1ed15d0df9595df4fde
[YuHan] <r11621208@ntu.edu.tw>
2023-10-24 18:59:39 +0800

    reconstruction package class complete

--- contours/draw_contours.py
+++ contours/draw_contours.py
@@ -1,112 +1,79 @@
 import os
 import re
-from typing import List
-import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from point import Point , Gradient
+from typing import List
 
 def read_contours(file):
-    x_li = []
-    y_li = []
-    global grad_lens
+    all_points = []
     with open(file, 'r') as f:
-        for row in f.readlines():
+        for row in f.readlines()    :
             result = re.search(r"(\d+)\s+(\d+)", row)
-            x, y = result.group(1), result.group(2)
-            x_li.append(x)
-            y_li.append(y)
-    return list(map(int, x_li)), list(map(int, y_li))
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+            if result:
+                x, y = map(int, result.groups())
+                p = Point(x, y)
+                all_points.append(p)
+    return all_points
 
-    def __repr__(self):
-        return f"({self.x}, {self.y})"
+def calculate_gradients(points: List[Point], interval: int = 1, move: int = 1) -> List[Gradient]:
+    gradients = [Gradient(points[i], points[i - interval]) for i in range(interval, len(points), move)]
+    return gradients    
 
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
+def plot_gradients(gradients: List[Gradient]):
+    gradient_values = [gradient.value for gradient in gradients]
+    x_values = list(range(len(gradient_values)))
+    plt.plot(x_values, gradient_values)
+    plt.xlabel('Point Index')
+    plt.ylabel('Gradient Value')
+    plt.show()
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
-
-    def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y)
-
-class Gradient():
-    GRAD_MAX = 20  # the maximum gradient value
-
-    def __init__(self, points: List[Point], interval: int= 1):
-        self.points = points
-        self.interval = interval
-        self.grad = []  # Added to store gradient values
-        
-
-    def calc_gradient(self):
-        for i in range(self.interval, len(self.points)):
-            if i - self.interval >= 0:
-                diff_x = self.points[i].x - self.points[i - self.interval].x
-                if diff_x == 0:
-                    result = Gradient.GRAD_MAX
-                else:
-                    diff_y = self.points[i].y - self.points[i - self.interval].y
-                    result = diff_y / diff_x
-                self.grad.append(result)
-
-
-
-
-def gradient(x, y, interval, output_name, output_csv_name):
-    points = [Point(x_val, y_val) for x_val, y_val in zip(x, y)]
-    grad_calculator = Gradient(points, interval=interval)
-    grad_calculator.calc_gradient()
-    grad = grad_calculator.grad
-    plt.plot(grad)
-    # Create a DataFrame from grad
-    df = pd.DataFrame({'Gradient': grad})
-
-    # Save the DataFrame to an Excel file
-    df.to_csv(output_csv_name, index=False)
-
-    plt.savefig(output_name)
-    plt.close()
-    return grad
-
-
-def get_all_file_paths(targetdir):
+p = read_contours(r"C:\Users\Lab_205\Desktop\image_overlapping_project\dataset_output\find_pattern\2_Chinese horse chestnut\contourfiles\1060_clear.csv")
+pgd = calculate_gradients(p,10,1)
+plot_gradients(pgd)
+def get_all_file_paths(directory):
     file_paths = []
-    for dirpath, _, filenames in os.walk(targetdir):
-        for file in filenames:
-            full_path = os.path.join(dirpath, file)
+    for filename in os.listdir(directory):
+        full_path = os.path.join(directory, filename)
+        if os.path.isfile(full_path):
             file_paths.append(full_path)
     return file_paths
 
+
+
+def output_gradvalue(gradlist: List[float],output_image_name: str,output_csv_name: str):
+    df = pd.DataFrame(gradlist, columns=['Gradient Value'])
+    df.to_csv(output_csv_name, index=False)
+    plt.plot(gradlist)
+    plt.xlabel('Point Index')
+    plt.ylabel('Gradient Value')
+    plt.savefig(output_image_name)
+    plt.close()
+
+
 def main():
-    targetdir = r'C:\Users\Lab_205\Desktop\image_processing_opencv\dataset_output\find_pattern\2_Chinese horse chestnut\contourfiles'
-    all_files = get_all_file_paths(targetdir)
-    output_dir = r'C:\Users\Lab_205\Desktop\image_processing_opencv\dataset_output\find_pattern\2_Chinese horse chestnut\gradientplot_20'
+    targetdir = r'C:\Users\Lab_205\Desktop\image_overlapping_project\dataset_output\find_pattern\2_Chinese horse chestnut\contourfiles'
+    output_dir = r'C:\Users\Lab_205\Desktop\testoutput'
+    
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
+    all_files = get_all_file_paths(targetdir)
+    
     for csvfile in all_files:
-        x_li, y_li = read_contours(csvfile)
+        points = read_contours(csvfile)
+        gradients = calculate_gradients(points, interval=1, move=1)
+
         base_name = os.path.basename(csvfile)
         plotname, _ = os.path.splitext(base_name)
+
+        output_csv_name = os.path.join(output_dir, f"{plotname}.csv")
         output_image_name = os.path.join(output_dir, f"{plotname}.jpg")
 
-        output_csv_name=os.path.join(output_dir, f"{plotname}.csv")
-        gradient(x_li, y_li, 15 , output_image_name,output_csv_name)
+        gradient_values = [gradient.value for gradient in gradients]
+        output_gradvalue(gradient_values, output_image_name, output_csv_name)
 
-    # Display the image (You can uncomment this part if needed)
-    # points = np.array([x_li, y_li], dtype=np.int32).T
-    # draw = np.zeros([512, 512], dtype=np.uint8)
-    # contours_site = cv.drawContours(draw, [points], -1, (255, 255, 255), thickness=1)
-    # plt.imshow(draw, cmap='gray')
-    # plt.show()
 
 if __name__ == "__main__":
     main()
---
+++ contours/point.py
@@ -0,0 +1,40 @@
+from typing import List
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+    
+
+class Gradient:
+    GRAD_MAX = 20  # the maximum gradient value
+
+    def __init__(self, start_point: Point, end_point: Point):
+        self.start_point = start_point
+        self.end_point = end_point
+        self.value = self.calc_gradient()
+
+    def calc_gradient(self) -> float:
+        delta = self.end_point - self.start_point
+        if delta.x == 0:
+            return Gradient.GRAD_MAX
+        else:
+            return delta.y / delta.x
+
+    def __repr__(self) -> str:
+        return f"{self.value}"
+
