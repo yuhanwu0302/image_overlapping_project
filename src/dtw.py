2f32ba614b76715a2a8e382ced654a316cfd6bb9
[YuHan] <r11621208@ntu.edu.tw>
2024-02-29 16:25:34 +0800

    modify dtw function structure

--- src/dtw.py
+++ src/dtw.py
@@ -1,136 +1,125 @@
-import numpy as np
-from fastdtw import fastdtw 
-from scipy.spatial.distance import euclidean
-import csv
-import numba as nb
-from tqdm.auto import tqdm
-import time 
-import os 
 import glob
+import os
+from itertools import product
 
+import numpy as np
+import pandas as pd
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
+from tqdm.auto import tqdm
+from collections import defaultdict
 #2  1060~ 1122
 #5  1195~ 1267
 #18 2347~ 2423
 
 np.set_printoptions(suppress = True)
 
-def clean_up_rows(r):
-    r = r.strip()
-    if r:
-        try:
-            return float(r)
-        except ValueError:
-            return None
-
-def read_file_to_array(file_path):
-    values = []
-    with open(file_path, "r") as f:
-        for row in f:
-            cleaned_value = clean_up_rows(row)
-            if cleaned_value is not None:  
-                values.append(cleaned_value)
-    array = np.array(values)
-    values_array = array.reshape(-1, 1)  
-    return values_array
-    
-
-def dis(dic,grad_number1:str,grad_number2:str):
-    distance, _ = fastdtw(dic[grad_number1], 
-    dic[grad_number2],dist=euclidean)
-    return distance
-
-def fetch_files(folder, ids):
-    files_paths = []
-    for i in ids:
-        file_path = glob.glob(f"{folder}/{i}*.csv")
-        files_paths.extend(file_path)
-    return files_paths
+def run():
+    # dir_1 = r"C:\Users\Lab_205\Desktop\image_overlapping_project\src\contours\test\grad\grad_rotated180"
+    # dir_2 = r"C:\Users\Lab_205\Desktop\image_overlapping_project\src\contours\test\grad\grad_rotated150"
 
 
-def save_array_to_dict(filepath,ids): 
-    grad_dict = {}
-    for file,ids in zip(filepath,ids):
-        grad_array = read_file_to_array(file)
-        grad_dict[f'grad_{ids}'] = grad_array
-    
-    return grad_dict
+    ### TODO: need change dir name  ###
 
+    "../dataset_output/find_pattern/{leaf_name}/contourfiles/grad/{rotate_???}/"
+    leaf_names = ["2_Chinese horse chestnut"]
+    grad_rotates = ["rotate_90"]
+    all_gradient_dict = {}
+    for leaf_name in leaf_names:
+        for grad_rotate in grad_rotates:
+            image_path = fr"../dataset_output/find_pattern/{leaf_name}/contourfiles/grad/{grad_rotate}/" 
+            # image_path = fr"C:\Users\Lab_205\Desktop\image_overlapping_project\src\contours\test\{leaf_name}\{grad_rotate}"
+            file_list = get_all_file_path(image_path)
+            gradient_dict = get_all_gradients(file_list)
+            all_gradient_dict[f"{leaf_name}_{grad_rotate}"] = gradient_dict
+    print(all_gradient_dict["2_Chinese horse chestnut_rotate_90"])
+    selected_leaves_grads = ["2_Chinese horse chestnut_rotate_90"]
+    selected_ids = [str(id) + "_clear" for id in range(1060, 1065)]
+    pair_dist = calculate_pairwise_dist(all_gradient_dict, selected_leaves_grads, selected_ids)
 
-
-def create_dict_name(leaf_id, angle, grad):
-    return f"Test{leaf_id}_{angle}_{grad}"
-
-
-def calculate_distance(grad_dict,grad_ids):
-    num_grads = len(grad_ids)
-    test_array = np.zeros((num_grads,num_grads))
-    for i in range(num_grads):
-        for j in range(num_grads):
-            grad_id_i = grad_ids[i]  
-            grad_id_j = grad_ids[j]
-            distance = dis(grad_dict, f"grad_{grad_id_i}", f"grad_{grad_id_j}")
-            test_array[i, j] = distance
-            test_array[j, i] = distance
-
-    return test_array.round(0)
-
+    return pair_dist
 
 def get_all_file_path(image_path):    
-    file_list = []
-    for k, v in image_path.items():
-        try:
-            file_list.append(fetch_files(k,v))
-        except:FileNotFoundError
-    return file_list
+    return glob.glob(os.path.join(image_path, "*.csv"))
 
-def get_all_gradients(file_list):    
-    for f in file_list:
+# def create_dict_name(leaf_id, angle, grad):
+#     return f"Test{leaf_id}_{angle}_{grad}"
+
+
+def get_all_gradients(file_list):
+    gradient_dict = {}
+    for filepath in file_list:
         grad = "20"
-        dict_name = create_dict_name(leaf_id, angle, grad) #folder_imageID_otherInfo
-        array_dict[dict_name] = save_array_to_dict(f)
-    return array_dict
+        # TODO: rename the images so that we can know all the information from their basenames.
+        data_id = os.path.basename(filepath).strip(".csv")
+        gradient_dict[data_id] = load_gradients(filepath)
+    return gradient_dict
+
+def load_gradients(file_path):
+    with open(file_path, "r") as f:
+        gradients = np.array([str_to_float(row) for row in f.readlines()])
+
+    return gradients.reshape(-1, 1)
+
+def str_to_float(r):
+    # If error is raised, there should be problems in gradient calculations.
+    # So don't handle any exceptions here. If raised, fix the gradient calculation code and raise exceptions there. 
+    return float(r.strip())
+
+def calculate_pairwise_dist(all_gradient_dict, leaves_grads, ids):
+    combined = list(product(leaves_grads, ids))
+    matrix_size = len(combined)
+    # A 2D-dict
+    pairwise_dist = defaultdict(dict)
     
-def calculate_pairwise_dist(gradient_dict):
-    all_test_dict = {}    
-    # Calculate the pairwise dist matrix
-    all_test_dict[dict_name] = calculate_distance(array_dict,grad_ids) #FIXME        
-    return all_test_dict
+    for i, (leaf_grad_1, id_1) in enumerate(combined):
+        name_1 = f"{leaf_grad_1}_{id_1}"
+        # The distance should be 0
+        diagonal_dis = dis(all_gradient_dict[leaf_grad_1][id_1], all_gradient_dict[leaf_grad_1][id_1])
+        # TODO: Check why not 0 in some cases
+        # assert np.isclose(diagonal_dis, 0), f"The distance to itself should be 0, but get {diagonal_dis}"
+        pairwise_dist[name_1][name_1] = diagonal_dis
 
-file_list = get_all_file_path(image_path) # All file (name/path) you want to work with
+        for j, (leaf_grad_2, id_2) in enumerate(combined):
+            if j >= i:
+                continue
+            name_2 = f"{leaf_grad_2}_{id_2}"
+            print(name_1, name_2)
+            pairwise_dist[name_1][name_2] = dis(all_gradient_dict[leaf_grad_1][id_1], all_gradient_dict[leaf_grad_2][id_2])
+            pairwise_dist[name_2][name_1] = pairwise_dist[name_1][name_2]
+    
+    return pairwise_dist
+            
+def dis(grad_1, grad_2):
+    distance, _ = fastdtw(grad_1, grad_2,dist=euclidean)
+    return distance
 
-    # for pytest
-    # file_list = ["f1.csv", "f2.csv"]
-gradient_dict = get_all_gradients(file_list) # get all gradients from these files {"image_id": gradient[]}
+#     all_test_dict = {}    
+#     # Calculate the pairwise dist matrix
+#     all_test_dict[dict_name] = calculate_distance(array_dict,selected_ids) #FIXME        
+#     return all_test_dict
 
+# file_list = get_all_file_path(image_path) # All file (name/path) you want to work with
 
-    # for pytest
-    # gradient_dict = {
-    #            "image_id_1": [1,2,3], 
-    #            "image_id_2": [2,4,6,7,9]
-    #}
-pair_dist = calculate_pairwise_dist(gradient_dict) #  
-
-
-
-
-
-
-def run():
-    all_test_dict = {}
-    test_id = "6"
-    parent_folder =    r"C:\Users\Lab_205\Desktop\image_overlapping_project\src\contours\test_4"
-    PRIFEX = "upand"
-    grad_ids = [i for i in range(1061,1069)]
-    image_path = {"upand45": grad_ids,
-                   "upand90": grad_ids,
-                   "upand180": [i for i in range(1061,1080)]
-                  }
-    image_path_v2 = {"45": grad_ids,
-                   "90": grad_ids,
-                   "180": [i for i in range(1061,1080)]
-                  } # key: angle/folder
+#     # for pytest
+#     # file_list = ["f1.csv", "f2.csv"]
+# gradient_dict = get_all_gradients(file_list) # get all gradients from these files {"image_id": gradient[]}
 
 
+#     # for pytest
+#     # gradient_dict = {
+#     #            "image_id_1": [1,2,3], 
+#     #            "image_id_2": [2,4,6,7,9]
+#     #}
+# selected_ids = [""]
+# pair_dist = calculate_pairwise_dist(gradient_dict) #  
+
+
+result = run()
+df = pd.DataFrame(result)
+df
+
+result["grad_grad_rotated180_1060_clear"]
 
 
 for _ in tqdm(range(1)):
@@ -145,11 +134,12 @@ all_test_array_3 # up 45 and up 90
 keys = ["Test6_1_2", 'Test6_2_2', "Test6_3_2", "Test6_4_2", "Test6_5_2","Test6_6_2"]
 labels = ["Test6_1_2", 'Test6_2_2_45', "Test6_3_2_90", "Test6_4_2_135", "Test6_5_2_150","Test6_6_2_180"]
 
+import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
 from scipy.stats import norm
 from sklearn.neighbors import KernelDensity
+
 for key , label in zip(keys,labels):
     test_y =[]
     for i in all_test_array_3[key]:
