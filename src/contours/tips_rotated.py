d8102ba0717583c58202d722662d79650d3ddf2c
[YuHan] <r11621208@ntu.edu.tw>
2024-03-07 17:10:42 +0800

    complete dtw.py

--- src/contours/tips_rotated.py
+++ src/contours/tips_rotated.py
@@ -117,10 +117,10 @@ def output_rotated_img_csv(output_csv_name:str,retaed_contour):
 
 #############################   run data    ######################
 def main():       
-    targetdir = r'C:\Users\Lab_205\Desktop\image_overlapping_project\src\dataset_output\all_data\contourfiles'
-    output_dir = r'C:\Users\Lab_205\Desktop\image_overlapping_project\src\contours\test'
+    targetdir = r'C:\Users\Lab_205\Desktop\image_overlapping_project\dataset_output\all_data\contourfiles'
+    output_dir = r'C:\Users\Lab_205\Desktop\image_overlapping_project\dataset_output\all_data\all_correct'
     
-    output_rotate_image_dir= r'C:\Users\Lab_205\Desktop\image_overlapping_project\src\contours\test_2\rotated_image'
+    output_rotate_image_dir= r'C:\Users\Lab_205\Desktop\image_overlapping_project\dataset_output\all_data\all_correct'
 
     if not os.path.exists(output_rotate_image_dir):
         os.makedirs(output_rotate_image_dir)
@@ -160,15 +160,15 @@ def main():
 
         output_csv_name = os.path.join(output_dir, f"{plotname}.csv")
         output_image_name = os.path.join(output_dir, f"{plotname}.jpg")
-        output_rotated_image_name = os.path.join(output_rotate_image_dir, f"{plotname}_rotated.jpg")
-        output_rotated_csv_name = os.path.join(output_rotate_image_dir, f"{plotname}_rotated.csv")
+        # output_rotated_image_name = os.path.join(output_rotate_image_dir, f"{plotname}_rotated.jpg")
+        # output_rotated_csv_name = os.path.join(output_rotate_image_dir, f"{plotname}_rotated.csv")
         output_gradvalue(gradient_values, output_image_name, output_csv_name)
-        rotaed_img = draw(clockwise)
-        output_rotated_img(rotaed_img,output_rotated_image_name)
-        output_rotated_img_csv(output_rotated_csv_name,clockwise)
+        # rotaed_img = draw(clockwise)
+        # output_rotated_img(rotaed_img,output_rotated_image_name)
+        # output_rotated_img_csv(output_rotated_csv_name,clockwise)
 
 
-
+main()
 
 '''check which part is which part
 interval = 10
--- src/dtw.py
+++ src/dtw.py
@@ -8,33 +8,26 @@ from fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 from tqdm.auto import tqdm
 from collections import defaultdict
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.neighbors import KernelDensity
 #2  1060~ 1122
 #5  1195~ 1267
 #18 2347~ 2423
 
-np.set_printoptions(suppress = True)
 
-def run():
-    # dir_1 = r"C:\Users\Lab_205\Desktop\image_overlapping_project\src\contours\test\grad\grad_rotated180"
-    # dir_2 = r"C:\Users\Lab_205\Desktop\image_overlapping_project\src\contours\test\grad\grad_rotated150"
+
+def run(leaf_name, grad_rotate, dir,selected_ids):
 
 
     ### TODO: need change dir name  ###
-
-    "../dataset_output/find_pattern/{leaf_name}/contourfiles/grad/{rotate_???}/"
-    leaf_names = ["2_Chinese horse chestnut"]
-    grad_rotates = ["rotate_90"]
+    np.set_printoptions(suppress = True)
     all_gradient_dict = {}
-    for leaf_name in leaf_names:
-        for grad_rotate in grad_rotates:
-            image_path = fr"../dataset_output/find_pattern/{leaf_name}/contourfiles/grad/{grad_rotate}/" 
-            # image_path = fr"C:\Users\Lab_205\Desktop\image_overlapping_project\src\contours\test\{leaf_name}\{grad_rotate}"
-            file_list = get_all_file_path(image_path)
-            gradient_dict = get_all_gradients(file_list)
-            all_gradient_dict[f"{leaf_name}_{grad_rotate}"] = gradient_dict
-    print(all_gradient_dict["2_Chinese horse chestnut_rotate_90"])
-    selected_leaves_grads = ["2_Chinese horse chestnut_rotate_90"]
-    selected_ids = [str(id) + "_clear" for id in range(1060, 1065)]
+
+    file_list = get_all_file_path(dir)
+    gradient_dict = get_all_gradients(file_list)
+    all_gradient_dict[rf"{leaf_name}_{grad_rotate}"] = gradient_dict
+    selected_leaves_grads = ["detect_diff_rotate_90"]
     pair_dist = calculate_pairwise_dist(all_gradient_dict, selected_leaves_grads, selected_ids)
 
     return pair_dist
@@ -42,15 +35,9 @@ def run():
 def get_all_file_path(image_path):    
     return glob.glob(os.path.join(image_path, "*.csv"))
 
-# def create_dict_name(leaf_id, angle, grad):
-#     return f"Test{leaf_id}_{angle}_{grad}"
-
-
 def get_all_gradients(file_list):
     gradient_dict = {}
     for filepath in file_list:
-        grad = "20"
-        # TODO: rename the images so that we can know all the information from their basenames.
         data_id = os.path.basename(filepath).strip(".csv")
         gradient_dict[data_id] = load_gradients(filepath)
     return gradient_dict
@@ -62,27 +49,20 @@ def load_gradients(file_path):
     return gradients.reshape(-1, 1)
 
 def str_to_float(r):
-    # If error is raised, there should be problems in gradient calculations.
-    # So don't handle any exceptions here. If raised, fix the gradient calculation code and raise exceptions there. 
     return float(r.strip())
 
 def calculate_pairwise_dist(all_gradient_dict, leaves_grads, ids):
     combined = list(product(leaves_grads, ids))
-    matrix_size = len(combined)
     # A 2D-dict
     pairwise_dist = defaultdict(dict)
     
-    for i, (leaf_grad_1, id_1) in enumerate(combined):
+    for _, (leaf_grad_1, id_1) in enumerate(combined):
         name_1 = f"{leaf_grad_1}_{id_1}"
         # The distance should be 0
         diagonal_dis = dis(all_gradient_dict[leaf_grad_1][id_1], all_gradient_dict[leaf_grad_1][id_1])
-        # TODO: Check why not 0 in some cases
-        # assert np.isclose(diagonal_dis, 0), f"The distance to itself should be 0, but get {diagonal_dis}"
         pairwise_dist[name_1][name_1] = diagonal_dis
 
-        for j, (leaf_grad_2, id_2) in enumerate(combined):
-            if j >= i:
-                continue
+        for _, (leaf_grad_2, id_2) in enumerate(combined):
             name_2 = f"{leaf_grad_2}_{id_2}"
             print(name_1, name_2)
             pairwise_dist[name_1][name_2] = dis(all_gradient_dict[leaf_grad_1][id_1], all_gradient_dict[leaf_grad_2][id_2])
@@ -94,12 +74,6 @@ def dis(grad_1, grad_2):
     distance, _ = fastdtw(grad_1, grad_2,dist=euclidean)
     return distance
 
-#     all_test_dict = {}    
-#     # Calculate the pairwise dist matrix
-#     all_test_dict[dict_name] = calculate_distance(array_dict,selected_ids) #FIXME        
-#     return all_test_dict
-
-# file_list = get_all_file_path(image_path) # All file (name/path) you want to work with
 
 #     # for pytest
 #     # file_list = ["f1.csv", "f2.csv"]
@@ -115,55 +89,56 @@ def dis(grad_1, grad_2):
 # pair_dist = calculate_pairwise_dist(gradient_dict) #  
 
 
-result = run()
-df = pd.DataFrame(result)
-df
-
-result["grad_grad_rotated180_1060_clear"]
-
-
-for _ in tqdm(range(1)):
-    all_test_array = run()
-
-
-all_test_array_1  #pointup
-all_test_array_2  #45
-all_test_array_3 # up 45 and up 90
-
 ########## plot  ##########
-keys = ["Test6_1_2", 'Test6_2_2', "Test6_3_2", "Test6_4_2", "Test6_5_2","Test6_6_2"]
-labels = ["Test6_1_2", 'Test6_2_2_45', "Test6_3_2_90", "Test6_4_2_135", "Test6_5_2_150","Test6_6_2_180"]
 
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
-from scipy.stats import norm
-from sklearn.neighbors import KernelDensity
 
-for key , label in zip(keys,labels):
-    test_y =[]
-    for i in all_test_array_3[key]:
-        for j in i:
-            test_y.append(j)    
+def plot_kde_and_CI(values,CI:int, compare_n_values=10 ,compare=False):
+    value = np.array(values).reshape(-1,1)
 
     kde = KernelDensity(bandwidth=1.0, kernel='gaussian')
-    kde.fit(np.array(test_y).reshape(-1, 1))
- 
-    x = np.linspace(min(test_y), max(test_y), 1000).reshape(-1, 1)
+    kde.fit(value)
+
+    x = np.linspace(min(value), max(value), 1000).reshape(-1, 1)
     pdf = np.exp(kde.score_samples(x))
     cdf = np.cumsum(pdf) * (x[1] - x[0])
 
-    confidence_level = 0.95
+    confidence_level = CI
     lower_bound = x[np.argmax(cdf >= (1-confidence_level)/2)][0]
     upper_bound = x[np.argmax(cdf >= 1-(1-confidence_level)/2)][0]
 
+    value_1d = value.flatten()
+    p = sns.histplot(value_1d, kde=True,label='All Data', color='blue')
+    if compare:
+        compare_value = value_1d[-compare_n_values:]
+        sns.histplot(compare_value, kde=False, color='red', label='Last 10 Data', bins=30, alpha=0.7)
 
-    p= sns.histplot(test_y, kde=True)
-    plt.title(label)
+    plt.title('test_detect_diff')
     plt.axvline(lower_bound, color="r", linestyle="--", label=f"{confidence_level*100}% CI")
     plt.axvline(upper_bound, color="r", linestyle="--")
     p.set_xlabel("distance")
     p.set_ylabel("freq")
+    plt.xlim(0,1800)
     plt.legend()
     plt.show()
     print(f"95% Confidence Interval: ({lower_bound}, {upper_bound})")
+
+
+
+
+
+for _ in tqdm(range(1)):
+    leaf_names=["detect_diff"]
+    grad_rotates =["rotate_90"]
+    selected_ids = [str(id) + "_clear" for id in list(range(1195, 1220)) + list(range(2114, 2119))]
+    for leaf_name in leaf_names:
+        for grad_rotate in grad_rotates:
+                image_path = fr"../dataset_output/find_pattern/{leaf_name}/contourfiles/grad/{grad_rotate}/" 
+                result1 = run(leaf_name,grad_rotate,image_path,selected_ids)
+df1 = pd.DataFrame(result1)
+
+values =[]
+for i in df1.values:
+    for j in i:
+        values.append(j)
+
+plot_kde_and_CI(values,0.95,6,True)
