df70092ffb464713dec5c32c211ca5f117486a9a
[YuHan] <r11621208@ntu.edu.tw>
2024-06-24 13:17:30 +0800

    temp modify

--- src/area_calculate/ans_area.py
+++ src/area_calculate/ans_area.py
@@ -2,20 +2,28 @@ import cv2 as cv
 import numpy as np
 import os
 import pandas as pd 
-from src.contours.draw_contours import read_contours
-idlist = [i for i in range(7001, 7019)]
+from contours.draw_contours import read_contours
 
+#### whole area calculation
+idlist = [i for i in range(7000, 7073)]
+ans_whloe_dic = {}
 
-con = pd.read_csv(r"C:\Users\Lab_205\Desktop\overlapping\1-overlapping_image\clear\contourfiles\7000_overlapping_clear.csv",header=None)
+for i in idlist:
+    con = pd.read_csv(fr"C:\Users\Lab_205\Desktop\overlapping_1\clear\contourfiles\{i}_clear.csv",header=None)
 
-con.drop([1], axis=1, inplace=True)
-con = con.T
-conlist = [i for i in con.iloc[0]]
-contour_points_list =[]
-for point_str in conlist:
-    point = list(map(int, point_str.strip('[]').split()))
-    contour_points_list.append(point)
-contour_points = np.array(contour_points_list, dtype=np.int32)
-contour_points = contour_points.reshape((-1, 1, 2))
-area = cv.contourArea(contour_points)
-print(area)
+    con.drop([1], axis=1, inplace=True)
+    con = con.T
+    conlist = [i for i in con.iloc[0]]
+    contour_points_list =[]
+    for point_str in conlist:
+        point = list(map(int, point_str.strip('[]').split()))
+        contour_points_list.append(point)
+    contour_points = np.array(contour_points_list, dtype=np.int32)
+    contour_points = contour_points.reshape((-1, 1, 2))
+    area = cv.contourArea(contour_points)
+    ans_whloe_dic[i] = area
+ans_whloe_dic
+df = pd.DataFrame(list(ans_whloe_dic.items()), columns=['id', 'whole_area'])
+df.to_csv(r'C:\Users\Lab_205\Desktop\ans_area.csv', index=False)
+
+###### down area calculation
